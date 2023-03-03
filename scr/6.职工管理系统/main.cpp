0f6931816e4de9f46fd3aced8a2b1648695bc938
Like2021 <1026569947@qq.com>
2023-03-03 15:06:02 +0800

    "修改tasks.json，但F5依旧无法调试"

--- scr/6.职工管理系统/main.cpp
+++ scr/6.职工管理系统/main.cpp
@@ -2,44 +2,52 @@
 using namespace std;
 #include"workerManager.h"
 
+#include"worker.h"
+#include"employee.h"
+
 int main()
 {
-    WorkerManager wm;
+    // 测试代码
+    Worker * worker = NULL;
+    worker = new Employee(1, "Like", 1);
+    worker->showInfo();
 
-    int choice = 0;
-    while(true)
-    {
-        // 展示菜单
-        wm.Show_Menu();
-        cout << "请输入您的选择" << endl;
-        cin >> choice;
+    // WorkerManager wm;
 
-        switch(choice)
-        {
-            case 0:  // 退出系统
-            wm.exitSystem();
-            break;
-            case 1:  // 添加职工
-            break;
-            case 2:  // 显示职工
-            break;
-            case 3:  // 删除职工
-            break;
-            case 4:  // 修改职工
-            break;
-            case 5:  // 查找职工
-            break;
-            case 6:  // 排序职工
-            break;
-            case 7:  // 清空职工
-            break;
-            default:
-            system("clear");
-            break;
-        }
-    }
+    // int choice = 0;
+    // while(true)
+    // {
+    //     // 展示菜单
+    //     wm.Show_Menu();
+    //     cout << "请输入您的选择" << endl;
+    //     cin >> choice;
 
-    wm.Show_Menu();
+    //     switch(choice)
+    //     {
+    //         case 0:  // 退出系统
+    //         wm.exitSystem();
+    //         break;
+    //         case 1:  // 添加职工
+    //         break;
+    //         case 2:  // 显示职工
+    //         break;
+    //         case 3:  // 删除职工
+    //         break;
+    //         case 4:  // 修改职工
+    //         break;
+    //         case 5:  // 查找职工
+    //         break;
+    //         case 6:  // 排序职工
+    //         break;
+    //         case 7:  // 清空职工
+    //         break;
+    //         default:
+    //         system("clear");
+    //         break;
+    //     }
+    // }
+
+    // wm.Show_Menu();
 
     system("read -p 'Press Enter to continue...' var");
 
