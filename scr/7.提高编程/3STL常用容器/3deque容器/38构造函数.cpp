763c281baaa202c1d376edf434e9fb2956d2f6fc
Like2021 <1026569947@qq.com>
2023-04-06 15:30:42 +0800

    2023.4.6

---
+++ scr/7.提高编程/3STL常用容器/3deque容器/38构造函数.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+#include<deque>
+
+// deque 构造函数
+
+void printDeque(const deque<int>&d)  // 加个const 限定成只读状态
+{
+    for(deque<int>::const_iterator it = d.begin(); it != d.end(); it++)
+    {
+        // *it = 100;  容器中的数据不可以修改了
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+void test01()
+{
+    deque<int>d1;
+    for(int i = 0; i < 10; i++)
+    {
+        d1.push_back(i);
+    }
+    printDeque(d1);
+
+    deque<int>d2(d1.begin(), d1.end());
+    printDeque(d2);
+
+    deque<int>d3(10, 100);
+    printDeque(d3);
+
+    deque<int>d4(d3);
+    printDeque(d4);
+}
+
+int main()
+{
+    test01();
+
+    // system("read -p 'Press Enter to continue...' var");
+
+    // return 0;
+
+}
